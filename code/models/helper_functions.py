5a8d74a9c756f9a429d0550bf22f853e8ca0ef1e
Louis <louis.berthele@gmail.com>
2024-11-30 16:17:29 +0100

    extract helper functions, make template model notebook, make rfe notebook

---
+++ code/models/helper_functions.py
@@ -0,0 +1,188 @@
+# Imports
+
+
+# Missing values imputer function with a given algorithm
+def impute_missing_values(data, target_column, algorithm):
+    
+    # Separating the missing values from the non missing values
+    available_data = data[data[target_column].notna()]
+    missing_data = data[data[target_column].isna()]
+
+    # Making sure there is enough data to input 
+    if len(available_data) == 0 or len(missing_data) == 0:
+        return data
+
+    # Separating the target column from the rest 
+    X_available = available_data.drop(columns=[target_column])
+    y_available = available_data[target_column]
+
+    # Training the model with the available data
+    model = algorithm
+    model.fit(X_available, y_available)
+
+    # Prediting the missing values
+    X_missing = missing_data.drop(columns=[target_column])
+    predicted_values = model.predict(X_missing)
+
+    # Inputing the missing values with the predictions
+    data.loc[data[target_column].isna(), target_column] = predicted_values
+
+    return data
+
+# Outlier removal function
+def handle_outliers(data, target_column):
+    print("Hello")
+    # Detect outliers
+    # If outliers > 5%
+        # Cap outliers
+    # If outliers < 5%
+        # Remove outliers (entire rows)
+
+# Scaling function
+def scale_numerical(X_train, X_val, scaler):
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_val_scaled = scaler.transform(X_val)
+    return X_train_scaled, X_val_scaled
+
+# Ordinal encoder function
+def categorical_ordinal_encode(X_train, X_val, features):
+    for col in features:
+        print("placeholder")
+        # Todo
+
+# Categorical encoder function
+def categorical_prop_encode(X_train, X_val, features):
+    for col in features:
+        proportion = X_train[col].value_counts(normalize = True)  # Get the porportion of each category
+        X_train[col] = X_train[col].map(proportion)  # Map the porportions in the column
+        X_val[col] = X_val[col].map(proportion) #Do the same for the valid dataset
+
+# Structure TODO:
+# SKFold Loop
+    # Split into current train/valid
+
+    # Impute missing values
+
+    # Outlier removal on train (only numerical)
+
+    # Scaling (only numerical)
+
+    # Categorical Prop Encoding
+    # Categorical Ordinal Encoding
+
+    # Fit model on train
+    # Predict valid
+def cv_scores(model, X, y, num_inputing_algorithm= XGBRegressor() , cat_inputing_algorithm = XGBClassifier()):
+    # Takes as argument the model used, the predictors and the target. Splits the data using StratifiedKFold, and
+    # trains model using X and y. Then it returns the results obtained from the stratified cross validation'''
+    
+    skf = KFold(n_splits=5)
+    
+    # Generating the lists to store our results
+    precision_scores_train = []
+    precision_scores_val = []   
+    recall_scores_train = []  
+    recall_scores_val = []
+    f1_scores_train = []    
+    f1_scores_val = []
+    index = [f'Fold {i}' for i in range(1,6)]
+    index.append("Average")
+    
+    for train_index, test_index in skf.split(X, y):
+        # Dividing our data in validation and train
+        X_train, X_val = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_val = y.iloc[train_index], y.iloc[test_index]
+
+        #Filling num missing values
+        for column in selected_num_features:
+            impute_missing_values(X_train, column, num_inputing_algorithm)
+            impute_missing_values(X_val, column, num_inputing_algorithm)
+
+        #Filling cat missing values
+        impute_missing_values(X_train, "Alternative Dispute Resolution", cat_inputing_algorithm)
+        impute_missing_values(X_val, "Alternative Dispute Resolution", cat_inputing_algorithm)
+        
+        # Removing inconsistencies on the train
+        inconsistent = X_train[(X_train['Age at Injury'] > 80) | (X_train["Age at Injury"] < 16)].index
+        X_train.drop(inconsistent, inplace=True)
+        y_train.drop(inconsistent, inplace=True)
+
+
+        # Training the classification model
+        model.fit(X_train, y_train)
+        
+        # Making the predictions for the training and validation data
+        pred_train = model.predict(X_train)
+        pred_val = model.predict(X_val)
+        
+        # Calculating and storing the scores
+        precision_scores_train.append(precision_score(y_train, pred_train, average='macro'))
+        precision_scores_val.append(precision_score(y_val, pred_val, average='macro'))
+        recall_scores_train.append(recall_score(y_train, pred_train, average='macro'))
+        recall_scores_val.append(recall_score(y_val, pred_val, average='macro'))
+        f1_scores_train.append(f1_score(y_train, pred_train, average='macro'))
+        f1_scores_val.append(f1_score(y_val, pred_val, average='macro'))
+
+    
+    precision_scores_train.append(mean(precision_scores_train))
+    precision_scores_val.append(mean(precision_scores_val))
+    recall_scores_train.append(mean(recall_scores_train))
+    recall_scores_val.append(mean(recall_scores_val))
+    f1_scores_train.append(mean(f1_scores_train))
+    f1_scores_val.append(mean(f1_scores_val))
+
+    # Storing the results in a dataframe
+    model_results = pd.DataFrame(data={
+        'Train_precision': precision_scores_train,
+        'Test_precision': precision_scores_val,
+        'Train_recall': recall_scores_train,
+        'Test_recall': recall_scores_val,
+        'Train_f1_score': f1_scores_train,
+        'Test_f1_score': f1_scores_val,
+    }, index=index)
+    
+    return model_results
+
+def test_prediction(model, X, y , test, num_inputing_algorithm= XGBRegressor() , cat_inputing_algorithm = XGBClassifier()):
+
+    X_train, X_val,y_train, y_val = train_test_split(X,y,
+                                                train_size = 0.8, 
+                                                shuffle = True, 
+                                                stratify = y)
+
+    # Missing value inputation
+    #Filling num missing values
+    for column in selected_num_features:
+        impute_missing_values(X_train, column, num_inputing_algorithm)
+        impute_missing_values(X_val, column, num_inputing_algorithm)
+        impute_missing_values(test, column, num_inputing_algorithm)
+
+    #Filling cat missing values
+    impute_missing_values(X_train, "Alternative Dispute Resolution", cat_inputing_algorithm)
+    impute_missing_values(X_val, "Alternative Dispute Resolution", cat_inputing_algorithm)
+    impute_missing_values(test, "Alternative Dispute Resolution", cat_inputing_algorithm)
+
+    # Removing inconsistencies on the train
+    inconsistent = X_train[(X_train['Age at Injury'] > 80) | (X_train["Age at Injury"] < 16)].index
+    X_train.drop(inconsistent, inplace=True)
+    y_train.drop(inconsistent, inplace=True)
+    
+    # Fitting the model
+    model.fit(X_train, y_train)
+
+    # Veryfing if the model is performing as expected
+    pred_val = model.predict(X_val)
+    print(f1_score(y_val, pred_val, average='macro'))
+
+    # Using the model to make prediction on the test dataset
+    pred_test = model.predict(test)
+
+    # Inversing the encoding of our target variable 
+    pred_test = le.inverse_transform(pred_test)
+
+    # Making a dataframe with the indexes of data_test and predictions converted back to strings
+    submission_df = pd.DataFrame({
+        "Claim Injury Type": pred_test
+    }, index=data_test.index)
+    
+    return submission_df
